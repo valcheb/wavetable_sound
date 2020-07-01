a948d2b85d33fa4faf8901889e2d053601db1ac2
valcheb <visel7@yandex.ru>
2020-07-01 09:24:43 +0300

    Move ring to separate module

---
+++ wavetable_synthesis/ring.h
@@ -0,0 +1,55 @@
+#ifndef RING_H_
+#define RING_H_
+
+#include "stdint.h"
+
+typedef struct ring
+{
+    uint8_t *buffer;
+    uint8_t in;
+    uint8_t out;
+    uint16_t size;
+    uint16_t current_size;
+} ring_t;
+
+inline static void ring_init(ring_t *rbuf, uint8_t *src_buf, uint16_t size)
+{
+    rbuf->buffer = src_buf;
+    rbuf->in = 0;
+    rbuf->out = 0;
+    rbuf->size = size;
+    rbuf->current_size = 0;
+}
+
+inline static void ring_put(ring_t *rbuf, uint8_t data)
+{
+    rbuf->buffer[rbuf->in++] = data;
+    if (rbuf->in >= rbuf->size)
+        rbuf->in = 0;
+    ++rbuf->current_size;
+}
+
+inline static uint8_t ring_pop(ring_t *rbuf)
+{
+    uint8_t res = rbuf->buffer[rbuf->out++];
+    if (rbuf->out >= rbuf->size)
+        rbuf->out = 0;
+    --rbuf->current_size;
+    return res;
+}
+
+inline static bool ring_is_full(ring_t *rbuf)
+{
+    if (rbuf->current_size >= rbuf->size)
+        return true;
+    return false;
+}
+
+inline static bool ring_is_empty(ring_t *rbuf)
+{
+    if (rbuf->current_size == 0)
+        return true;
+    return false;
+}
+
+#endif /*RING_H_*/
--- wavetable_synthesis/wavetable_synth.c
+++ wavetable_synthesis/wavetable_synth.c
@@ -1,57 +1,6 @@
 #include "wavetable_synth.h"
+#include "ring.h"
 
-//ring buffer interface
-
-typedef struct ring
-{
-    uint8_t *buffer;
-    uint8_t in;
-    uint8_t out;
-    uint16_t size;
-    uint16_t current_size;
-} ring_t;
-
-inline static void ring_init(ring_t *rbuf, uint8_t *src_buf, uint16_t size)
-{
-    rbuf->buffer = src_buf;
-    rbuf->in = 0;
-    rbuf->out = 0;
-    rbuf->size = size;
-    rbuf->current_size = 0;
-}
-
-inline static void ring_put(ring_t *rbuf, uint8_t data)
-{
-    rbuf->buffer[rbuf->in++] = data;
-    if (rbuf->in >= rbuf->size)
-        rbuf->in = 0;
-    ++rbuf->current_size;
-}
-
-inline static uint8_t ring_pop(ring_t *rbuf)
-{
-    uint8_t res = rbuf->buffer[rbuf->out++];
-    if (rbuf->out >= rbuf->size)
-        rbuf->out = 0;
-    --rbuf->current_size;
-    return res;
-}
-
-inline static bool ring_is_full(ring_t *rbuf)
-{
-    if (rbuf->current_size >= rbuf->size)
-        return true;
-    return false;
-}
-
-inline static bool ring_is_empty(ring_t *rbuf)
-{
-    if (rbuf->current_size == 0)
-        return true;
-    return false;
-}
-
-//wavetable and raw interface
 #define COOK_SIZE 32
 static uint8_t  cooked_data[COOK_SIZE];
 static ring_t   data_ring;
