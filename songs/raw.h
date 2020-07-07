1bfb37d0320c4e59a0ae968086f7aa3682c83748
valcheb <visel7@yandex.ru>
2020-07-07 08:19:45 +0300

    Delete raw player from project

--- songs/raw.h
+++
@@ -1,9 +0,0 @@
-#ifndef RAW_H_
-#define RAW_H_
-
-#include "stdint.h"
-
-#define SONG_LEN 12128
-extern const uint8_t raw[];
-
-#endif /*RAW_H_*/
--- wavetable_synthesis/player.c
+++ wavetable_synthesis/player.c
@@ -1,23 +1,12 @@
 #include "player.h"
 
-//#define PLAY_RAW
-
-#ifdef PLAY_RAW
-# include "raw_player.h"
-#else
-# include "wavetable_synth.h"
-#endif
+#include "wavetable_synth.h"
 
 void pl_init()
 {
     ma_init_mcu();
-#ifdef PLAY_RAW //вместо кучи ifdef можно использовать структуру с указателями на функции
-    raw_init();
-    ma_set_expand(6); //mcu_rate/song_rate
-#else
     wts_init();
-    ma_set_expand(1);
-#endif
+    ma_set_expand(1); //TODO delete func
 }
 
 void pl_start_play()
@@ -33,12 +22,6 @@ void pl_stop_play()
 
 void pl_play_song() //TODO rewrite with argument but reduce global variables
 {
-#ifdef PLAY_RAW
-    if (ma_is_data_needed())
-    {
-        ma_pwm_play(raw_get_value());
-    }
-#else
     if (!wts_is_full()) //is_cook_continue && !is_full
     {
         wts_cook_data();  //буфер заполняется COOK_SIZE лишних раз. на воспроизведение raw не влияет
@@ -48,14 +31,9 @@ void pl_play_song() //TODO rewrite with argument but reduce global variables
     {
         ma_pwm_play(wts_get_value());
     }
-#endif
 }
 
 bool pl_is_continue()
 {
-#ifdef PLAY_RAW
-    return raw_is_continue();
-#else
     return wts_is_continue();
-#endif
 }
